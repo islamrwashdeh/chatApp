0d1e5f1a7647aa118e94b608d4bfeab4ac6b2a67
salma <islamrwashdeh@yahoo.com>
2024-03-06 14:50:47 +0300

    chat continer setup

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,6 +9,7 @@ const router = require("express").Router();
 
 router.post("/login", login);
 router.post("/register", register);
+router.get("/allusers/:id", getAllUsers);
 router.post("/setavatar/:id", setAvatar);
 
 module.exports = router;
